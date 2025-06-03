4f9c15582d37c15e9465547b9eba12c3b5c1b197
Daniyal Siddiqui <160016275+daniyalsiddiqui123@users.noreply.github.com>
2025-06-03 22:30:24 +0500

    Update page.tsx

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,8 +1,8 @@
 export default function Download() {
     return(
         <div>
-        <h1 className="text-7xl text-white animate-pulse font-bold text-center p-7">Under-Maintenance</h1>
-        <h1 className="text-2xl text-white font-semibold text-center">Thank-You For Your Understanding</h1>
+        <h1 className="text-3xl sm:text-7xl text-white animate-pulse font-bold text-center p-7">Under-Maintenance</h1>
+        <h1 className="text-xl sm:text-2xl text-white font-semibold text-center">Thank-You For Your Understanding</h1>
         </div>
     )
 }
