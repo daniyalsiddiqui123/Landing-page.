ee94ad9bb155a365b99ce7f9c93d1395fb91a50c
Daniyal Siddiqui <160016275+daniyalsiddiqui123@users.noreply.github.com>
2025-06-03 22:30:49 +0500

    Update border.tsx

--- src/app/border.tsx
+++ src/app/border.tsx
@@ -1,6 +1,6 @@
 export default function Border() {
     return (
-        <div className="h-[600px] sm:h-72 w-[600px] 2xl:hidden">
+        <div className="h-[600px] sm:h-72 2xl:hidden">
             <h1></h1>
         </div>
     )
