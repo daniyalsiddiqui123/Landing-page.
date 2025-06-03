00412cb8a63d735237fd063466f90dfd2046f91d
Daniyal Siddiqui <160016275+daniyalsiddiqui123@users.noreply.github.com>
2025-06-03 22:31:07 +0500

    Update borderTwo.tsx

--- src/app/borderTwo.tsx
+++ src/app/borderTwo.tsx
@@ -1,6 +1,6 @@
 export default function BorderTwo() {
     return (
-        <div className="h-screen sm:hidden md:hidden w-[600px] 2xl:hidden">
+        <div className="h-screen sm:hidden md:hidden 2xl:hidden">
             <h1></h1>
         </div>
     )
