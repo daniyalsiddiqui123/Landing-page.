d59bb890395a5f81cd361b9bd3ce7a915618e7b0
Daniyal Siddiqui <160016275+daniyalsiddiqui123@users.noreply.github.com>
2025-06-04 10:51:47 +0500

    Update page.tsx

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,6 +1,6 @@
 export default function Download() {
     return(
-        <div>
+        <div className="bg-black h-screen">
         <h1 className="text-3xl sm:text-7xl text-white animate-pulse font-bold text-center p-7">Under-Maintenance</h1>
         <h1 className="text-xl sm:text-2xl text-white font-semibold text-center">Thank-You For Your Understanding</h1>
         </div>
