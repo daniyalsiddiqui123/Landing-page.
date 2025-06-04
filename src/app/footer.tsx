567389f9b81ff51c4c8e930b020e60115316b9ce
Daniyal Siddiqui <160016275+daniyalsiddiqui123@users.noreply.github.com>
2025-06-04 11:01:15 +0500

    Update footer.tsx

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -6,7 +6,7 @@ import { FaSquareInstagram } from "react-icons/fa6";
 
 export default function Footer() {
     return (
-        <div className="bg-blue-950 h-screen sm:h-56 md:h-72 2xl:h-[600px]">
+        <div className="bg-blue-950 h-screen sm:h-56 md:h-72 2xl:h-[600px] text-white">
             <h1 className="p-20 sm:p-10 2xl:text-3xl">© 2025 Flowly. All rights reserved.</h1>
             <div className="flex-wrap items-center space-x-20 sm:text-right mx-5 sm:mx-10 md:text-left md:flex -my-32 md:-my-20 p-16 2xl:-mx-7 2xl:my-0.5">
                 <Link href='/'><button className="cursor-pointer hover:text-green-400 2xl:text-3xl">Privacy Policy</button></Link>
@@ -14,7 +14,7 @@ export default function Footer() {
                 <Link href='/'><button className="cursor-pointer hover:text-green-400 2xl:text-3xl">Cookie Preferences </button></Link>
             </div>
             <div>
-                <div className="flex text-left space-x-10 text-2xl p-10 my-20 lg: 2xl:text-3xl">
+                <div className="flex text-left space-x-10 text-2xl p-10 my-20 lg: 2xl:text-3xl text-white">
                     <Link href='/'><FaYoutube className="cursor-pointer hover:text-red-500" /></Link>
                     <Link href='/'><FaSquareXTwitter className="cursor-pointer hover:text-blue-500" /></Link>
                     <Link href='/'><FaFacebook className="cursor-pointer hover:text-blue-700" /></Link>
